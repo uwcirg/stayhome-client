5f1b8dec20a65ef9244ba562e7a57f88c7fb82a6
Amy Chen <clone@uw.edu>
2020-04-10 16:13:27 -0700

    Service worker runtime error fix

--- web/service-worker.js
+++ web/service-worker.js
@@ -11,6 +11,29 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', function(event) {
-//  console.log('👷', 'fetch', event);
-  event.respondWith(fetch(event.request));
+  //  console.log('👷', 'fetch', event);
+  //  event.respondWith(fetch(event.request));
+  event.respondWith(fetch(event.request).then(function(response) {
+    if (!response.ok) {
+      // An HTTP error response code (40x, 50x) won't cause the fetch() promise to reject.
+      // We need to explicitly throw an exception to trigger the catch() clause.
+      throw Error('response status ' + response.status);
+    }
+
+    // If we got back a non-error HTTP response, return it to the page.
+    return response;
+  }).catch(function(error) {
+    console.warn('Constructing a fallback response, ' +
+      'due to an error while fetching the real response:', error);
+
+    // return error response as fallback.
+    var fallbackResponse = {
+      error: "fetching response failed"
+    };
+
+    // Construct the fallback response via an in-memory variable.
+    return new Response(JSON.stringify(fallbackResponse), {
+      headers: {'Content-Type': 'application/json'}
+    });
+  }));
 });
